d46462d48e6fac7b007a15635d02e2c5f580baf9
DnlDt <danielgranosik2@gmail.com>
2026-05-25 22:18:20 -0300

    a

--- 1a/01 js/0030 SQ06/script.js
+++ 1a/01 js/0030 SQ06/script.js
@@ -1,4 +1,4 @@
-let resposta = document.getElementById("resultado")
+let resposta_ganso = document.getElementById("resultado")
 function ganso(){
     let marujos, comida,quantidade_marujo, dias_mais
     faltam_comida = 1.5
@@ -7,19 +7,76 @@ function ganso(){
     comida = Number(prompt("Quantidade de comida(KG):"))
     quantidade_marujo = comida /marujos
     if(marujos >=10 && quantidade_marujo >=1.5){
-        resposta.innerHTML = "Provisões suficientes. Rumo ao horizonte!"
+        resposta_ganso.innerHTML = "Provisões suficientes. Rumo ao horizonte!"
         if(quantidade_marujo >1.5){
-            dias_mais = quantidade_marujo/ 1.5
-            resposta.innerHTML += "Você pode ficar mais " + dias_mais + "dias com a comida que sobrou "
+            dias_mais = Math.floor(quantidade_marujo/ 1.5)
+            dias_mais = dias_mais -1 
+            resposta_ganso.innerHTML += "<br>Você pode ficar mais " + dias_mais + " dias com a comida que sobrou "
         }
     }else{
-         resposta.innerHTML = "Algo está errado. Posseidom não quer ninguém no mar hoje."
+         resposta_ganso.innerHTML = "<br>Algo está errado. Posseidom não quer ninguém no mar hoje."
          if(marujos <10){
             faltam_marujos = faltam_marujos - marujos
-            resposta.innerHTML += "<br>Você precisa de mais " + faltam_marujos + "marujos para zarpar"
-         }else if(quantidade_marujo <1.5){
+            resposta_ganso.innerHTML += "<br>Você precisa de mais " + faltam_marujos + " marujos para zarpar"
+         }if(quantidade_marujo <1.5){
             faltam_comida = faltam_comida - quantidade_marujo
-            resposta.innerHTML += "<br>Você precisa de mais " + faltam_comida + "KG de comida por marujo para zarpar"
-         }
+            resposta_ganso.innerHTML += "<br>Você precisa de mais " + faltam_comida + " KG de comida por marujo para zarpar"
+         }  
+    }
+}
+function recruta(){
+    let nadar = (document.getElementById("nadar").value)
+    let idade = parseInt(document.getElementById("idade").value)
+    let carta = (document.getElementById("carta").value)
+    if((nadar == "S" || nadar == "Sim" || nadar == "sim" || nadar == "s") && idade >16){ // botar os parentes para nao pular o &&
+        document.getElementById("resposta-recruta").innerHTML = "Aprovado para o navio"
+    }else if((carta == "Sim" || carta == "s" || carta == "S" || carta == "sim") && idade >= 14 ){
+        document.getElementById("resposta-recruta").innerHTML = "Aprovado sob recomendação. Monitorar nas primeiras viagens"
+    }else{
+        document.getElementById("resposta-recruta").innerHTML = "Reprovado. Volte quando estiver mais preparado para o mar"
+    }
+}
+function saru_maqui(){
+    let criancas = parseInt(document.getElementById("criancas").value)
+    let velocidad = parseFloat(document.getElementById("velocidade").value)
+    if(criancas == 0 && velocidade >=0.5){
+        document.getElementById("resposta-fumaca").innerHTML = "Fumaça liberada. Que comecem os mistérios!"
+    }else{
+        document.getElementById("resposta-fumaca").innerHTML = "Fumaça bloqueada. Aguardar condições ideais"
+    }
+}
+function saru_deslocamento(){
+    let gripado = (document.getElementById("gripado").value)
+    let tempo = parseInt(document.getElementById("tempo").value)
+    if((gripado == "N" || gripado == "n" || gripado == "Não" || gripado == "nao" || gripado == "Nao") && tempo <45){
+        document.getElementById("resposta-deslocamento").innerHTML = "Viagem autorizada. Rumo ao show!"
+    }else{
+        document.getElementById("resposta-deslocamento").innerHTML = "Deslocamento inviável. Melhor recusar este evento."
+    }
+}
+function saru_entrada(){
+    let som = (document.getElementById("som").value)
+    let figurino = (document.getElementById("figurino").value)
+    if ((som == "S" || som == "s" || som == "Sim" || som == "sim") && (figurino == "S" || figurino == "s" || figurino == "sim" || figurino == "Sim")){
+        document.getElementById("resposta-palco").innerHTML = "Palco liberado para Sarumano! Luzes, câmera, ilusão!"
+    }else if((som == "N" || som == "n" || som == "Não" || som == "nao" || som == "não") && (figurino == "S" || figurino == "s" || figurino == "sim" || figurino == "Sim")){
+        document.getElementById("resposta-palco").innerHTML = "Sem som. Realizar apresentação alternativa no salão."
+    }else if((som == "S" || som == "s" || som == "Sim" || som == "sim") && (figurino == "N" || figurino == "n" || figurino == "Não" || figurino == "nao" || figurino == "não")){
+        document.getElementById("resposta-palco").innerHTML = "Faltando parte do figurino. Te vira no improviso!"
+    }else if((figurino == "N" || figurino == "n" || figurino == "Não" || figurino == "nao" || figurino == "não") && (som == "N" || som == "n" || som == "Não" || som == "nao" || som == "não")){
+        document.getElementById("resposta-palco").innerHTML = "Apresentação cancelada. Hora da mágica de desaparecer"
+    }else{
+        document.getElementById("resposta-palco").innerHTML = "Por favor, digite novamente"
+    }
+}
+function Deny(){
+    let manuntencao = (document.getElementById("manuntencao").value.toLowerCase().trim()) //tolowercase deixa a resposta em minusculo, .trim remove os espaços
+    let emergencia = (document.getElementById("emergencia").value.toLowerCase().trim())
+    if((manuntencao == "n" || manuntencao == "nao") && ( emergencia == "n" || emergencia == "nao")){
+        document.getElementById("resposta_d").innerHTML = "Transporte liberado para uso."
+    }else if(manuntencao == "S" || manuntencao == "s" || manuntencao == "sim" || emergencia == "s" || emergencia == "sim"){
+        document.getElementById("resposta_d").innerHTML = "Transporte indisponível por motivo de segurança"
+    }else{
+        document.getElementById("resposta_d").innerHTML = "Por favor digite novamente"  
     }
 }
--- 1a/04-variaveis/script.js
+++ 1a/04-variaveis/script.js
@@ -1,5 +1,76 @@
 let cont = 0
 let total = 0
+let totalrolado = 0
+let contd2 = 0
+let contd4 = 0
+
+function resetar(){
+    totalrolado = 0
+    document.getElementById("outputultima").innerHTML = ""
+    document.getElementById("outputtotal").innerHTML = 0
+    document.getElementById("historico").innerHTML = ""
+}
+function rolardado(numero){
+    let dado = Math.ceil(Math.random()*numero)
+    document.getElementById("historico").innerHTML += "<span>d" + numero + ": " + dado + "</span>"
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+
+
+
+
+
+
+function rolard2(){
+    let dado = Math.ceil(Math.random()*2)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+function rolard4(){
+    let dado = Math.ceil(Math.random()*4)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+function rolard6(){
+    let dado = Math.ceil(Math.random()*6)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+function rolard10(){
+    let dado = Math.ceil(Math.random()*10)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+function rolard12(){
+    let dado = Math.ceil(Math.random()*12)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+function rolard20(){
+    let dado = Math.ceil(Math.random()*20)
+    document.getElementById("outputultima").innerHTML = dado
+    totalrolado += dado
+    document.getElementById("outputtotal").innerHTML = totalrolado
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function registrarvenda(){
     let valor = Number(document.getElementById("inputVenda").value)
     console.log(valor)
@@ -8,17 +79,7 @@ function registrarvenda(){
     document.getElementById("listaVendas").innerHTML += "R$" + valor.toFixed(2) + "<br>"
     document.getElementById("inputVenda").value = ""
     document.getElementById("inputVenda").focus()
-}
-function teste(){
-    
-}
-
-
-
-
-
-
-
+}  
 
 
 
@@ -48,11 +109,10 @@ function sub10(){
     cont = cont -10
     document.getElementById("p-contagem").innerHTML = cont
 }
-
-
 let global = "Sou uma variavel global"
 // function funcao1(){
 //     let local = "Sou uma variavel local *()*"
 //     console.log(global)
 //     console.log(local);
 // }
+
