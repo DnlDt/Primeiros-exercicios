d473dc01db7ff1b8f3de1f1c60a361bf821eff97
DnlDt <danielgranosik2@gmail.com>
2026-06-16 22:14:43 -0300

    a

---
+++ 1a/01 js/Repeticao-0065/script.js
@@ -0,0 +1,90 @@
+function quadrado(){
+    let contador = 0
+    while(contador <10){
+        contador ++
+        let quadrado = contador * contador
+        alert(contador + "² = " + quadrado)
+    }
+}
+function regressiva(){
+    let contador = 10
+    while(contador>0){
+        alert(contador)
+        contador = contador - 1
+    }
+}
+function soma(){
+    let total = 0
+    let contador = 0 
+    while(contador<100){
+        contador ++ 
+        total = total + contador
+    }
+    alert(total)
+}
+function tabuada(){
+    let tabu = 0
+    let contador = 0 
+    while(contador<10){ 
+        contador ++
+        tabu = 5 * contador
+        alert("5X" + contador + " = " + tabu)
+    }
+}
+function tabuada_usuario(){
+    let tabu = Number(prompt("Numero da tabuada:")) 
+    let resultado = 0
+    let contador = 0 
+    while(contador<tabu){
+        contador ++
+        resultado = tabu * contador
+        alert(tabu +"X" + contador + " = " + resultado)
+    }
+}
+function impar(){ // 6
+    let contador = 0 
+    let impar
+    while(contador<20){
+        contador ++
+        if(contador % 2 != 0){
+            alert("O numero " + contador + " é impar")
+        }
+    }
+}
+function crescente(){
+    let contador = 0 
+    let contado = 10
+    while(contador<10){
+        contador ++
+        alert(contador)
+    }
+    while(contado >0){
+        contado --
+        alert(contado)
+    }
+}
+function fatorial(){ // incompleto
+    let fatora = Number(prompt("Numero:"))
+    let resultado 
+    while(fatora>0){
+        p = fatora - 1
+        resultado = fatora * p
+
+    }
+    alert(resultado)
+}
+
+function maluquice(){
+    c = 0
+    while(c<100){
+        c++
+        if(c %3 == 0 && c %5 == 0){
+            alert( c + "FizzBuzz")
+        }else if(c %3 == 0){
+            alert( c + "Fizz")
+        }else if(c%5 == 0){
+            alert(c + "Buzz")
+    }
+}
+    alert(c)
+}
--- 1a/06-repeticao/script.js
+++ 1a/06-repeticao/script.js
@@ -7,10 +7,20 @@
 
 
 
-let peso
-let contador = 0
-peso = Number(prompt("Digite o peso:"))
-if(peso>1.1 || peso<0.9){
-    alert("fora tolerancia do peso ")
+// let peso
+// let contador = 0
+// peso = Number(prompt("Digite o peso:"))
+// if(peso>1.1 || peso<0.9){
+//     alert("fora tolerancia do peso ")
+// }
+// contador ++
+
+let total = 0
+let i = 0
+while(i < 3){
+    i++
+    let preco = Number(prompt("Digite o preço do " + i + "° produto"))
+    total = total + preco
+
+    document.getElementById("total").innerHTML += i + "° produto: R$" + preco.toFixed(2) + "<br>"
 }
-contador ++
